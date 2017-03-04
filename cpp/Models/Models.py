4a1886340b3d851b919b4152116590f5248ea5b7
EliezerMdz <eliezermdz7@gmail.com>
2017-03-04 13:08:03 -0400

    Cambios minimos a los modelos

--- cpp/Models/Models.py
+++ cpp/Models/Models.py
@@ -1,29 +1,32 @@
-from django.db import models
-
-class usuarios(models.model):
-    nombre = models.CharField(max_length = 50)
-    apellido = models.CharField(max_length = 50)
-    nombre_usuario = models.CharField(max_length = 50)
-    clave = models.CharField(max_length = 50)
+from Django.db import models
 
 
-class Conceptos_pago(models.Model):
-    descripcion = models.CharField(max_length = 50)
+class Usuarios(models.model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    nombre_usuario = models.CharField(max_length=50)
+    clave = models.CharField(max_length=50)
+
+
+class Conceptos_Pago(models.Model):
+    descripcion = models.CharField(max_length=50)
     estado = models.BooleanField()
 
+
 class Proveedores(models.Model):
-    nombre = models.CharField(max_length = 50)
+    nombre = models.CharField(max_length=50)
     tipo_persona = models.BooleanField()
-    cedula_rnc = models.CharField(max_length = 11)
+    cedula_rnc = models.CharField(max_length=11)
     balance = models.IntegerField()
     estado = models.BooleanField()
 
+
 class Facturas (models.Model):
-    numero_factura = models.CharField(max_length = 50)
+    numero_factura = models.CharField(max_length=50)
     fecha_factura = models.DateField()
-    monto = models.DecimalField(max_digits = 11, decimal_places = 2)
+    monto = models.DecimalField(max_digits=11, decimal_places=2)
     fecha_registro = models.DateField()
-    proveedor = models.CharField(max_length = 50)
+    proveedor = models.CharField(max_length=50)
     estado = models.BooleanField()
 
 
